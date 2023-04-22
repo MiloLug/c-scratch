4bf87edea00c9cc9e2b8b589f965d7dbfac36cbe
Lia Va <mistickusya.2012@gmail.com>
2023-04-22 16:16:46 +0200

    add autopublish

---
+++ collect_npm_zip.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+origin=zip_npm_base
+destination=zip_npm_tmp
+package_name=c-scratch-base-project
+package_version=`npm view c-scratch-base-project version`
+
+rm -rf "$destination"
+cp -r "$origin" "$destination"
+
+sed -E \
+  -e 's/\{\{package_version\}\}/'"$package_version"'/' \
+  -e 's/\{\{package_name\}\}/'"$package_name"'/' \
+  <"$origin/package.json" > "$destination/package.json"
+
+mkdir -p "$destination/dist/project"
+mkdir -p "$destination/dist/project/sprites"
+
+cp -r runtime .gitignore CMakeLists.txt config.h "$destination/dist/project/"
+zip -r "$destination/dist/project.zip" "$destination/dist/project/"*
