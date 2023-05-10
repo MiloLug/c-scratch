2f6a37db4c1dbcd3b3b22cbcf0bf055352d675b3
Lia Va <mistickusya.2012@gmail.com>
2023-05-10 12:41:06 +0200

    revert some changes from the keys commit

--- runtime/string.h
+++ runtime/string.h
@@ -29,7 +29,7 @@ public:
     bool shouldMove = false;  // hint to not make any copies and just take the pointer
     bool isWrapper = false;  // delete is forbidden in any case, force copying on `copy` etc
 
-    static String * create(const wchar_t * value) {
+    static inline String * create(const wchar_t * value) {
         String * self = (String *)malloc(sizeof(String));
         self->shouldMove = false;
         self->isWrapper = false;
@@ -39,7 +39,7 @@ public:
         return self;
     }
 
-    static String * create(String & value) { return value.copy(); }
+    static inline String * create(String & value) { return value.copy(); }
 
     static double __parseHexNum(double & sign, const wchar_t * str) {
         if (str[0] == L'0') {
--- runtime/string_utils.h
+++ runtime/string_utils.h
@@ -5,30 +5,34 @@
 #include "value.h"
 
 #define __str_make_bool_op(op)                                                                     \
-    bool operator op(String && s1, String && s2) { return wcscmp(s1.data, s2.data) op 0; }         \
-    bool operator op(const wchar_t * s1, String && s2) { return wcscmp(s1, s2.data) op 0; }        \
-    bool operator op(String && s1, const wchar_t * s2) { return wcscmp(s1.data, s2) op 0; }        \
-    bool operator op(String && s1, double s2) {                                                    \
+    inline bool operator op(String && s1, String && s2) { return wcscmp(s1.data, s2.data) op 0; }  \
+    inline bool operator op(const wchar_t * s1, String && s2) { return wcscmp(s1, s2.data) op 0; } \
+    inline bool operator op(String && s1, const wchar_t * s2) { return wcscmp(s1.data, s2) op 0; } \
+    inline bool operator op(String && s1, double s2) {                                             \
         return wcscmp(s1.data, toTmpString(s2).data) op 0;                                         \
     }                                                                                              \
-    bool operator op(double s1, String && s2) {                                                    \
+    inline bool operator op(double s1, String && s2) {                                             \
         return wcscmp(toTmpString(s1).data, s2.data) op 0;                                         \
     }                                                                                              \
-    bool operator op(String && s1, int s2) { return wcscmp(s1.data, toTmpString(s2).data) op 0; }  \
-    bool operator op(int s1, String && s2) { return wcscmp(toTmpString(s1).data, s2.data) op 0; }
+    inline bool operator op(String && s1, int s2) {                                                \
+        return wcscmp(s1.data, toTmpString(s2).data) op 0;                                         \
+    }                                                                                              \
+    inline bool operator op(int s1, String && s2) {                                                \
+        return wcscmp(toTmpString(s1).data, s2.data) op 0;                                         \
+    }
 
 
 static constexpr String toTmpString(const wchar_t * s1) {
     return String(std::char_traits<wchar_t>::length(s1), (wchar_t *)s1, true, true);
 }
-static const String & toTmpString(const String & s1) { return s1; }
-static String toTmpString(Value & s1) {
+static inline const String & toTmpString(const String & s1) { return s1; }
+static inline String toTmpString(Value & s1) {
     if (s1.type == Value::Type::STRING)
         return String(s1.string->length, s1.string->data, true, true);
 
     return String(s1.numberStrSize - 1, s1.getNumberStr(), true, true);
 }
-static String toTmpString(double s1) {
+static inline String toTmpString(double s1) {
     Value tmp(s1);
     wchar_t * tmpStr = tmp.getNumberStr();
     tmp.numberStrTmp = NULL;
@@ -51,23 +55,23 @@ static String join(const String & s1, const String & s2) {
     return String(s1.length + s2.length, res, true);
 }
 template<typename T1, typename T2>
-static String join(T1 && s1, T2 && s2) {
+static inline String join(T1 && s1, T2 && s2) {
     return join(toTmpString(s1), toTmpString(s2));
 }
 
 
 static constexpr double lengthOf(const String & s1) { return s1.length; }
 template<typename T>
-static double lengthOf(T && s1) {
+static inline double lengthOf(T && s1) {
     return lengthOf(toTmpString(s1));
 }
 
 
-static String letterOf(const String & s1, uint64_t i) {
+static inline String letterOf(const String & s1, uint64_t i) {
     return i > 0 && i <= s1.length ? String(s1.data[i - 1], 1, true) : String();
 }
 template<typename T>
-static String letterOf(T && s1, uint64_t i) {
+static inline String letterOf(T && s1, uint64_t i) {
     return letterOf(toTmpString(s1), i);
 }
 
